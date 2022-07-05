fa45619be4826e7fc50b9277e5fbf0f54a1c0795
Uri Baynesay <uri552@gmail.com>
2022-07-05 09:21:45 +0300

    setup done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./assets/styles/main.scss"
+import { HomePage } from "./pages/HomePage";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <AppHeader /> */}
+      <main>
+        <HomePage/>
+      </main>
+      {/* <AppFooter /> */}
+    </div>
+  )
+}
+
+export default App
